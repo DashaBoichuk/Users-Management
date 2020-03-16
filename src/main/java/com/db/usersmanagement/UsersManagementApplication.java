2ec3e98eb98be54942521b3f6de1e7d56b6b10d4
Dasha Boichuk <dashaboichuk@gmail.com>
2020-03-16 05:06:59 +0200

    init commit. added db connect, liquibase change sets with db tables.

---
+++ src/main/java/com/db/usersmanagement/UsersManagementApplication.java
@@ -0,0 +1,13 @@
+package com.db.usersmanagement;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class UsersManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersManagementApplication.class, args);
+    }
+    }
+
